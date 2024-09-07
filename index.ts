9f926526d34555f295bf37ba6163d3dc847e89d8
lily <lilyhuang-github@users.noreply.github.com>
2024-09-06 21:57:17 -0400

    clean up

--- index.ts
+++ index.ts
@@ -1,29 +1,19 @@
 
-// import {Groq} from "groq-sdk";
-//groq api thing
+
 import yargs, { type Arguments } from "yargs";
 import {appendFile } from 'fs';
 import {OpenAI} from 'openai'; 
-//yargs, argument package thingy
 
 const {name, version} = require("./package.json");
 
-// const groq: Groq = new Groq({ apiKey: process.env.APIKEY });
-// if(process.env?.APIKEY){
-// const apiKey:string = process.env.APIKEY;
-// }
+
 const groqAPIKEY:string = process.env?.APIKEY ? process.env.APIKEY : "";
 const requireAPIKey:boolean = !!(groqAPIKEY);
 const gorg:string = "https://api.groq.com/openai/v1";
 const langModel:string = "llama3-8b-8192";
-export async function main() {
-    // const chatCompletion = await getGroqChatCompletion();
-    // Print the completion returned by the LLM.
-    // console.log(chatCompletion.choices[0]?.message?.content || "wrong");
-  }
+
 export async function getGroqChatCompletion(prompt:string, model:string = langModel, argapiKey:string =groqAPIKEY, urlThing:string =gorg): Promise<any> {
   //hypotehtically can translate to any language (next thing) but
-
   //new idea code obfuscation
   //want to do something else but this is what i thought of 
   // const 
@@ -44,16 +34,7 @@ export async function getGroqChatCompletion(prompt:string, model:string = langMo
   
 }
   )
-    // const thing = await groq.chat.completions.create({
-    //   messages: [
-    //     {
-    //       role: "user",
-    //       // content: `Can you convert this code to ${convert}: ${code}`,
-    //       content:prompt,
-    //     },
-    //   ],
-    //   model: model,
-    // });
+
     return thing.choices[0]?.message?.content;
   }
 //deal with argument stuff
@@ -62,12 +43,13 @@ const languageTranslation = async(things : string, lang : string,  model:string 
 //read the file of thing
 //shoot off error if not
 //alternative is a rubbery ducky but idk
+//other idea is for it to explain how the things are connected together
+
   try{
     const foo = Bun.file(things);
     const text = await foo.text();
     const prompt:string = `Can you convert this code to ${lang} and also in a pirate language: ${text}`;
     return await getGroqChatCompletion(prompt, model, apiKey, urlThing);
-  // return await getGroqChatCompletion(await foo.text(), lang);
   }catch(err)
   {console.error(`Error: ${err}`);
   throw err;
@@ -90,18 +72,9 @@ command('$0 <files...>', 'parses one to many files', (yargs) =>{
       demandOption:true
     });
 }, async (argv) => {
-  // console.log(argv);
   if(Array.isArray(argv.files)){
-    // console.log(argv?.files
-    
-    // if(argv?.o){
-    //   console.log("WOWA");
-    // }
+    // if(argv?.o){ vs typeof argv?.o = 'string' idk
     for(const file of argv.files){
-      // if(typeof argv.m == 'string'){
-      //   console.log("doing the thing");
-      //   const modelThing: string = argv.m || "llama3-8b-8192";
-      // }
       const m: string = typeof argv.m =='string' ? argv.m : langModel;
       const e: string = typeof argv.e =='string' ? argv.e : gorg;
       const k: string = typeof argv.k =='string' ? argv.k : groqAPIKEY;
@@ -111,13 +84,10 @@ command('$0 <files...>', 'parses one to many files', (yargs) =>{
         if(typeof argv.l === 'string'){
           //a check to make sure it works
 
-        
+
           if(typeof argv.o === 'string'){
          //a check but also it kind of checks if its exists or not cause otherwise it's unknown 
             if(typeof argv.a === 'boolean'){
-              //checks if the a flag exists so then it appends to file instead of overriding
-              //(things : string, lang : string,  model:string = langModel, apiKey:string =groqAPIKEY, urlThing:string =gorg) =>{
-//read the file of thing
                 appendFile(argv.o, await languageTranslation(file, argv.l, m, k,e), err =>{
                   if(err) throw err;
                 })
@@ -126,7 +96,6 @@ command('$0 <files...>', 'parses one to many files', (yargs) =>{
               await Bun.write(argv.o, await languageTranslation(file, argv.l, m, k,e));
             }
             //bun can't append to files even though they have this
-         // console.log(await languageTranslation(file, argv.l));
           }
           else{
             //if they're not writing to file
@@ -192,10 +161,3 @@ help('h', "shows the commands").
 alias('h', 'help').
 alias('v', 'version').
 parse();
-
-
-//console.dir(argv);
-  //console.log(x);
-
-//Bun.argv
-  //  main();
